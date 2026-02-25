a459785b5802a76cbafd34f48003aa160f9756cc
Aylla Scaglia <aylla@scaglia.com.br>
2026-02-25 19:57:10 -0300

    feat(todolist-api): configure postgres connection and create Task model

--- todolist-api/app/__init__.py
+++ todolist-api/app/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
 
     from .routes import main
     app.register_blueprint(main)
