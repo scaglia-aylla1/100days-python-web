744ac1b10e67c4fb6c0b1830a88be6db90afbd4f
Aylla Scaglia <aylla@scaglia.com.br>
2026-02-27 21:18:06 -0300

    refactor: allow test configuration injection

--- todolist-api/app/__init__.py
+++ todolist-api/app/__init__.py
@@ -5,16 +5,16 @@ import os
 
 db = SQLAlchemy()
 
-def create_app():
-    load_dotenv()
-
+def create_app(test_config=None):
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    if test_config:
+        app.config.update(test_config)
+    else:
+        app.config.from_object(Config)
 
     db.init_app(app)
 
-    from .models import Task
     from .routes import main
     app.register_blueprint(main)
 
